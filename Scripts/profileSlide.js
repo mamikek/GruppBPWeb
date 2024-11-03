4bd570c8eb9242ff5a631391e70d6d69527cd901
Mirko Martjak <mirko.martjak@ut.ee>
2024-11-03 13:42:58 +0200

    JSON script (Pooleli)
    Json JS osa. Pooleli on

--- Scripts/profileSlide.js
+++ Scripts/profileSlide.js
@@ -1,2 +1,17 @@
-//Gets Avatar location from HTML
-const Avatar = document.getElementsByClassName("Avatar");
+//For JSON server we choose Tartu University Adalberg file server.
+fetch('.\Test.json')
+        .then((response) => response.json())
+        .then(json => {
+            console.log(json);
+            
+            const pName = document.createElement("p");
+            const pEmail = document.createElement("p");
+            pName.innerHTML = `Username: ${json.name}`;
+            pEmail.innerHTML = `Email: ${json.email}`;
+            
+            const addToDrop = document.getElementById("dropdownMenu")
+
+            addToDrop.appendChild(pName);
+            addToDrop.appendChild(pEmail);
+        }
+    );
