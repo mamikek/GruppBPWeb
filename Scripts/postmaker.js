7642bd9e0abfa8969716b7bd9e0c8e6bd937a1f1
Mirko Martjak <mirko.martjak@ut.ee>
2024-11-03 23:08:50 +0200

    Post update

--- Scripts/postmaker.js
+++ Scripts/postmaker.js
@@ -5,7 +5,7 @@
 //LINK: https://jsonhost.com/json/e61f2b6a8d00a02fdb3a4d181ba10b0d
 
 window.onload = function() {
-    fetch("")
+    fetch("./posts.json")
             .then((response) => response.json())
             .then(json => {
                 console.log(json);
@@ -13,19 +13,45 @@ window.onload = function() {
                 //Needs for
                 for (const singleInfo of json) {
                     //Give constant and create text
-                    const pName = document.createElement("p");
-                    const pEmail = document.createElement("p");
+                    const Post = document.createElement("div");
+                    Post.classList.add("Post");
+                    const PostHead = document.createElement("div");
+                    PostHead.classList.add("PostHead");                    
+                    const MiniAvatar = document.createElement("div");
+                    MiniAvatar.classList.add("AvatarMini")
+                    const plot = document.createElement("div");
+                    plot.classList.add("Plot");
 
-                    //Put the json to places
-                    pName.innerHTML = `Username: ${singleInfo.name}`;
-                    pEmail.innerHTML = `Email: ${singleInfo.email}`;
+                    const ImageAvatar = document.createElement("img");
+
+                    const MainText = document.createElement("h2");
+                    MainText.classList.add("Anytext")
+
+                    const text = document.createElement("p");
+
+                    const PostImage = document.createElement("img");
+                    PostImage.classList.add("Picture")
+
+                    MainText.innerHTML = `${singleInfo.poster_name} ${singleInfo.creation_date} ${singleInfo.creation_time}`
+                    text.innerHTML = singleInfo.post_text;
+                    PostImage.innerHTML = singleInfo.post_media_path;
+                    ImageAvatar.innerHTML = singleInfo.poster_avatar_path;
+                    
+                    
+                    MiniAvatar.appendChild(ImageAvatar);
+                    PostHead.appendChild(MiniAvatar);
+                    PostHead.appendChild(MainText);
+                    Post.appendChild(PostHead);
+                    Post.appendChild(PostImage);
+                    Post.appendChild(text)
+                    Post.appendChild(plot);
                     
                     //Get the html where you want to place the text
-                    const addToDrop = document.getElementById("dropdownMenu")
+                    const Flex = document.getElementById("PostsPlace")
                     
-                    //Add info to the id parent and its done
-                    addToDrop.appendChild(pName);
-                    addToDrop.appendChild(pEmail);
+                    //add last things
+                    Flex.appendChild(Post);
+
                 }
             }
         );
