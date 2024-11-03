d2337d6af8f0d8ed42bd09feca619ec1db15da82
Mirko Martjak <mirko.martjak@ut.ee>
2024-11-03 21:26:25 +0200

    JS update

--- Scripts/profileSlide.js
+++ Scripts/profileSlide.js
@@ -1,17 +1,29 @@
 //For JSON server we choose Tartu University Adalberg file server.
-fetch('.\Test.json')
+//works on local files.
+
+//PLEASE USE THE LOCAL INSTEAD OF LINK BECAUSE OF DAILY REQUESTS
+//LINK: https://jsonhost.com/json/e61f2b6a8d00a02fdb3a4d181ba10b0d
+fetch("./Test.json")
         .then((response) => response.json())
         .then(json => {
             console.log(json);
             
-            const pName = document.createElement("p");
-            const pEmail = document.createElement("p");
-            pName.innerHTML = `Username: ${json.name}`;
-            pEmail.innerHTML = `Email: ${json.email}`;
-            
-            const addToDrop = document.getElementById("dropdownMenu")
+            //Needs for
+            for (const singleInfo of json) {
+                //Give constant and create text
+                const pName = document.createElement("p");
+                const pEmail = document.createElement("p");
 
-            addToDrop.appendChild(pName);
-            addToDrop.appendChild(pEmail);
+                //Put the json to places
+                pName.innerHTML = `Username: ${singleInfo.name}`;
+                pEmail.innerHTML = `Email: ${singleInfo.email}`;
+                
+                //Get the html where you want to place the text
+                const addToDrop = document.getElementById("dropdownMenu")
+                
+                //Add info to the id parent and its done
+                addToDrop.appendChild(pName);
+                addToDrop.appendChild(pEmail);
+            }
         }
     );
