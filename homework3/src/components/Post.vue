70590f9e51e842bde709c37fc80683db2e5c1c9d
random-T420 <162351240+random-T420@users.noreply.github.com>
2024-11-24 23:29:41 +0200

    added the posts

--- homework3/src/components/Post.vue
+++ homework3/src/components/Post.vue
@@ -2,13 +2,14 @@
     <div class="post">
       <h2>{{ title }}</h2>
       <p>{{ content }}</p>
+      <img v-if="showImage" :src={{ image }} alt="Post Image"/>
       <button @click="likePost">Like ({{ likes }})</button>
     </div>
   </template>
   
   <script>
   export default {
-    props: ['title', 'content', 'likes'],
+    props: ['title', 'content','image', 'likes'],
     methods: {
       likePost() {
         this.$emit('like');
