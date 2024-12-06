851907a0ddca58844a8b68e250b23f8900853bca
random-T420 <162351240+random-T420@users.noreply.github.com>
2024-12-06 14:33:57 +0200

    added basic login page

---
+++ homework3/src/views/LoginPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="signup-page"> <!--it seems fine to just use the old signup page styling-->
+        <!--figure out a way to verify if user exists in database, and logic for dealing with JWT-s -->
+      <form class="login-form" >
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="email" placeholder="Email" />
+        </div>
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" placeholder="Password" />
+        </div>
+        <button type="submit" class="login-button">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+
+  </script>
+  
