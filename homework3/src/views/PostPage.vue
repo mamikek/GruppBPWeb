256889d5bf77228e1ea36ffb69804d4a8c903f83
Mirko Martjak <mirko.martjak@ut.ee>
2024-12-06 14:32:50 +0200

    Post page and style update
    small update

--- homework3/src/views/PostPage.vue
+++ homework3/src/views/PostPage.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="post-page">
-        <h1>Make a post</h1>
-
+        <div class="PostingDiv">
+            <h1>Add Post</h1>
+            <label for="PostBody">Body: </label>
+            <input type="text" id="PostBody" v-model="text" placeholder="A Post Body" >
+            <!--Reusing signup-button style-->
+            <button class="signup-button">Submit the post</button>
+        </div>
+        
     </div>
 </template>
 
